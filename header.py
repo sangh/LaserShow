c994600038baaf85b9df809f770eb2fdfd4eb88b
San GH <san.gh@clockwatchin>
2011-06-29 20:10:30 -0400

    Added the constants.

--- header.py
+++ header.py
@@ -1,5 +1,19 @@
+"""
+The intent of this file is to be available from _every_ python file in this
+project, so it should probably be included as:
+    from header import *
+"""
 
+# Hard-coded things see the readme.
+XGridSize, YGridSize = 100, 100
+DelayOff, DelayOn = 10, 200
+DelayMove = 5
+DelayGlow = 20
+
+# Stuff everyone should have.
 import sys
+import os
+import re
 
 def wrn(s):
     print >> sys.stderr, "%s: %s"%(sys.argv[0].split('/')[-1], s)
