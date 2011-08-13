610b8d24902fa6b062361586e081e2a8a22dfce5
San GH <san.gh@clockwatching.net>
2011-08-12 17:49:07 -0700

    broken

--- textUI.py
+++ textUI.py
@@ -274,97 +274,40 @@ def cmdGlyphSlowSend( lc = None ):
     return glyphSendHelper( sendGlyphSlowXY, lc )
 
 def cmdGamePad( lc = None ):
-    def gamePadThreadFn(q):
-        try:
-            p = subprocess.Popen( [
-                "/usr/bin/jstest",
-                "--event",
-                "/dev/input/by-id/usb-Logitech_Logitech_RumblePad_2_USB-joystick",
-                ], shell=False, stdout=subprocess.PIPE )
-            while True:
-                q.put(p.stdout.readline(),False)
-        except:
-            print sys.exc_info()
-        finally:
-            try:
-                p.terminate()
-                p.kill()
-            except:
-                pass
-    gamePadQ = Queue.Queue(maxsize=100)
-    gamePadThread = threading.Thread(target=gamePadThreadFn,args=(gamePadQ,))
     keyBoardThread = threading.Thread(target=lambda: raw_input())
-    gamePadThread.daemon = True
     keyBoardThread.daemon = True
-    gamePadThread.start()
     keyBoardThread.start()
+    wrn("Press Enter to exit back to menu.")
+    try:
+        gamePadProc = subprocess.Popen( [
+            "/usr/bin/jstest",
+            "--event",
+            "/dev/input/by-id/usb-Logitech_Logitech_RumblePad_2_USB-joystick",
+            ], shell=False, stdout=subprocess.PIPE )
 
-    try: # Clear the queue of header, needs except if thread died.
-        while True: jq.get(True,.01)
-    except:
-        pass
-    # Lines from gamepad.
-    # Event: type 2, time 17339776, number 1, value 19255
-    r=re.compile("^Event: type ([0-9]), time ([0-9]+), number ([0-9]?[0-9]), value (-?[0-9]+)$")
-    # Until Enter is pressed.
-    while kt.isAlive() and jt.isAlive():
-        print "Press enter on the keyboard to go back to the menu."
-        try:
-            l = jq.get(True, .2)
-            rm = r.match(l)
-            if rm:
+        # Lines from gamepad.
+        # Event: type 2, time 17339776, number 1, value 19255
+        r=re.compile("^Event: type ([0-9]), time ([0-9]+), number ([0-9]?[0-9]), value (-?[0-9]+)$")
+        while keyBoardThread.isAlive():
+            line = gamePadProc.stdout.readline()
+            rm = r.match(line)
+            if not rm:
+                raise NameError("Line invalid: "+line.strip())
+            else:
                 t = int(rm.group(1))
                 ti = int(rm.group(2))
                 n = int(rm.group(3))
                 v = int(rm.group(4))
                 print l.strip()
                 print "t=%d  time=%d  num=%d  val=%d"%(t,ti,n,v)
-            else:
-                print "No.::%s::"%(l)
-                break
-        except Queue.Empty:
-            pass
-        except:
-            print sys.exc_info()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    try:
-        p = subprocess.Popen( [
-            "/usr/bin/jstest",
-            "--event",
-            "/dev/input/by-id/usb-Logitech_Logitech_RumblePad_2_USB-joystick",
-            ], shell=False, stdout=subprocess.PIPE )
-        print p.stdout.poll()
     except:
         wrn(sys.exc_info())
-    try:
-        p.terminate()
-        p.kill()
-    except:
-        pass
+    finally:
+        try:
+            p.terminate()
+            p.kill()
+        except:
+            pass
     return None
 
 def cmdPass( lc = None ):
